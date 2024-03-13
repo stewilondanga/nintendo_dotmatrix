50f0bbea221618a3a31fa7eb72eb85f8246ef2f5
Stewart <stilondanga@gmail.com>
2024-03-13 14:57:15 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,8 @@ const toggler = document.getElementById("start");
 const togglerAgain = document.getElementById("pause");
 const toggleSplash = document.getElementById("splash");
 const isHidden = () => toggleSplash.classList.contains("box--hidden");
-/*const audio = document.getElementById("audio");
-
+const audio = document.getElementById("audio");
+/*
 toggleSplash.addEventListener("transitionend", function () {
 	if (isHidden()) {
 		toggleSplash.style.display = "none";
