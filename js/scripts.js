ae20fe35c1d2a45022360c61363edfabccd79fd7
Stewart <stilondanga@gmail.com>
2024-03-13 14:55:20 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,8 @@ var navigate = (function() {
 
 const toggler = document.getElementById("start");
 const togglerAgain = document.getElementById("pause");
-/*const toggleSplash = document.getElementById("splash");
-const isHidden = () => toggleSplash.classList.contains("box--hidden");
+const toggleSplash = document.getElementById("splash");
+/*const isHidden = () => toggleSplash.classList.contains("box--hidden");
 const audio = document.getElementById("audio");
 
 toggleSplash.addEventListener("transitionend", function () {
