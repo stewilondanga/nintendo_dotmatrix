4e8d2e2a62560b5bf69253333c1a59ef19881509
Stewart <stilondanga@gmail.com>
2024-03-13 13:32:39 +0300

    add js/scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+/*
+var navigate = (function() {
+	$('.dd').toggle();
+	$('.dd_btn').click(function() {
+		var dataName = $(this).attr('data-name');
+		$('.dd').hide();
+		$('.' + dataName).toggle();
+	});
+})();
+
+const toggler = document.getElementById("start");
+const togglerAgain = document.getElementById("pause");
+const toggleSplash = document.getElementById("splash");
+const isHidden = () => toggleSplash.classList.contains("box--hidden");
+const audio = document.getElementById("audio");
+
+toggleSplash.addEventListener("transitionend", function () {
+	if (isHidden()) {
+		toggleSplash.style.display = "none";
+	}
+});
+
+toggler.addEventListener("click", function () {
+	if (isHidden()) {
+		toggleSplash.style.removeProperty("display");
+		setTimeout(() => toggleSplash.classList.remove("box--hidden"), 0);
+		audio.play();
+	} else {
+		toggleSplash.classList.add("box--hidden");
+		audio.pause();
+	}
+});
+
+togglerAgain.addEventListener("click", function () {
+	toggleSplash.classList.add("box--hidden");
+	audio.pause();
+});
+*/
